416c4f89b586de9a61a249d12f735c381ed8c865
João Marcelo <joaovictormarcelo5@gmail.com>
2025-08-24 10:59:21 -0300

    test project

---
+++ Test_Project/IntegrationTests/IntegrationHttpTest/HttpPostAndUpdateTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using Core.Request;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+using Xunit.Abstractions;
+using Assert = Xunit.Assert;
+
+namespace Test_Project.IntegrationTests.IntegrationHttpTest;
+
+public class HttpPostAndUpdateTest: IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly ITestOutputHelper _helper;
+
+    public HttpPostAndUpdateTest(WebApplicationFactory<Program> factory, ITestOutputHelper helper)
+    {
+        _client = factory.CreateClient();
+        _helper = helper;
+    }
+
+    [Fact]
+    public async Task Post()
+    {
+        var car = new CarDto
+        {
+            Carcolor = "azul",
+            CarModel = "fusca",
+            CarPrice = 5000,
+            sold = false
+        };
+        var content = new StringContent(JsonSerializer.Serialize(car), Encoding.UTF8,"application/json");
+       var response = await _client.PostAsync("/car",content);
+
+       Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Update()
+    {
+        var car = new CarDto
+        {
+            Carcolor = "preto",
+            CarModel = "fusca",
+            CarPrice = 4000,
+            sold = false
+        };
+        var content = new StringContent(JsonSerializer.Serialize(car), Encoding.UTF8,"application/json");
+        var response = await  _client.PutAsync("/car", content);
+        
+        Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+        _helper.WriteLine(response.ReasonPhrase);
+        
+        var json = await response.Content.ReadAsStringAsync();
+        
+        Assert.False(string.IsNullOrEmpty(json));
+    }
+}
---
+++ Test_Project/UnitTests/TestDBconnection.cs
@@ -0,0 +1,29 @@
+﻿using DDD_clean_architecture.Infrastructure;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Test_Project.UnitTests;
+
+public class TestDBconnection
+{
+   
+    
+    [Fact]
+    public void TestConnection()
+    {
+        var configurestring = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("C:\\Users\\User\\RiderProjects\\good_pratices\\Test_Project\\test.File.json", optional: false, reloadOnChange: true).Build();
+
+        var connectionstring = configurestring.GetConnectionString("Connection");
+
+        var options = new DbContextOptionsBuilder<Context>().UseSqlServer(new SqlConnection(connectionstring)).Options;
+
+        var context = new Context(options).Database.CanConnect();
+        
+        Assert.True(context);
+    }
+}
